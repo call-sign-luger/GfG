92363d9d500ea4d95e1c3a890661aabce13b024b
Vedansh Balooni <vedanshbalooni12@gmail.com>
2024-07-19 22:38:47 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Union of Two Linked Lists/union-of-two-linked-lists.cpp
@@ -0,0 +1,121 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+	int data;
+	struct Node* next;
+	
+	Node(int x){
+	    data = x;
+	    next = NULL;
+	}
+	
+};
+
+
+// } Driver Code Ends
+/*
+// structure of the node is as follows
+
+struct Node
+{
+	int data;
+	struct Node* next;
+	
+	Node(int x){
+	    data = x;
+	    next = NULL;
+	}
+	
+};
+
+*/
+class Solution
+{
+    public:
+    struct Node* makeUnion(struct Node* head1, struct Node* head2)
+    {
+        // code here
+        set<int> st;
+        Node* curr=head1;
+        while(curr){
+            st.insert(curr->data);
+            curr=curr->next;
+        }
+        curr=head2;
+        while(curr){
+            st.insert(curr->data);
+            curr=curr->next;
+        }
+        
+        Node* head=NULL;
+        curr=NULL;
+        for(auto i:st){
+        if(head==NULL){
+          head=new Node(i);
+          curr=head;
+        }
+        else{
+            curr->next=new Node(i);
+            curr=curr->next;
+        }
+        }
+        return head;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+struct Node* buildList(int size)
+{
+    int val;
+    cin>> val;
+    
+    Node* head = new Node(val);
+    Node* tail = head;
+    
+    for(int i=0; i<size-1; i++)
+    {
+        cin>> val;
+        tail->next = new Node(val);
+        tail = tail->next;
+    }
+    
+    return head;
+}
+
+void printList(Node* n)
+{
+    while(n)
+    {
+        cout<< n->data << " ";
+        n = n->next;
+    }
+    cout<< endl;
+}
+
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n, m;
+        
+        cin>>n;
+        Node* first = buildList(n);
+        
+        cin>>m;
+        Node* second = buildList(m);
+        Solution obj;
+        Node* head = obj.makeUnion(first,second);
+        printList(head);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
